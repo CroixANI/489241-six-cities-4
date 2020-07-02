a248c55355690cc415ba195d2aa45a212a92a726
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-03 01:28:18 +0300

    5.17.3 Обновил index.js.
    - Создал store
    - Обернул App в Provider
    - Передал Provider ссылку на store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import App from './components/app/app.jsx';
 import OFFERS from './mocks/offers';
+import reducer from './reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App offers={OFFERS.filter((offer) => offer.location.city === `Amsterdam`)} />,
+    <Provider store={store}>
+      <App offers={OFFERS.filter((offer) => offer.location.city === `Amsterdam`)} />
+    </Provider>,
     document.querySelector(`#root`)
 );
