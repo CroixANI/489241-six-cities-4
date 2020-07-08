d35334b4a2f2c4e56abc078fdb4ef2a0857080e0
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-09 02:43:37 +0300

    5.18.3 Поправил тесты и где нужно передал mock для store и использовал Provider.

--- src/components/app/app.e2e.test.js
+++ src/components/app/app.e2e.test.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import {App} from './app';
 import OffersCardsContainer from '../offers-cards-container/offers-cards-container.jsx';
 import {OFFERS_TESTS, CITIES_TESTS} from '../../mocks/offers-tests';
+import {SORT_TYPE} from '../../data/constants';
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
 
+const mockStore = configureStore([]);
+
 describe(`App Component`, () => {
+  const store = mockStore({
+    filteredOffers: OFFERS_TESTS,
+    sortType: SORT_TYPE.POPULAR
+  });
+
   it(`Should mouse over be triggered`, () => {
     const appComponent = mount(
-        <App offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} />
+        <Provider store={store}>
+          <App offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} />
+        </Provider>
     );
 
     const card = appComponent.find(`.place-card`).first();
@@ -24,10 +36,13 @@ describe(`App Component`, () => {
   });
 
   it(`Should offer title be clicked and change state`, () => {
-    const appComponent = mount(
-        <App offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} />
+    const wrapper = mount(
+        <Provider store={store}>
+          <App offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} />
+        </Provider>
     );
 
+    const appComponent = wrapper.find(App).first();
     const title = appComponent.find(`h2.place-card__name`).first();
 
     title.simulate(`click`, {});
--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import Main from './main';
 import OffersCardsContainer from '../offers-cards-container/offers-cards-container.jsx';
 import {OFFERS_TESTS, CITIES_TESTS} from '../../mocks/offers-tests';
+import {SORT_TYPE} from '../../data/constants';
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
 
+const mockStore = configureStore([]);
+const store = mockStore({
+  filteredOffers: OFFERS_TESTS,
+  sortType: SORT_TYPE.POPULAR
+});
+
 describe(`Main Screen`, () => {
   it(`Should mouse over be triggered`, () => {
     const mainScreen = mount(
-        <Main offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} onOfferTitleClick={() => {}} />
+        <Provider store={store}>
+          <Main offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} onOfferTitleClick={() => {}} />
+        </Provider>
     );
 
     const card = mainScreen.find(`.place-card`).first();
@@ -27,7 +38,9 @@ describe(`Main Screen`, () => {
     const onOfferTitleClick = jest.fn();
 
     const mainScreen = mount(
-        <Main offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} onOfferTitleClick={onOfferTitleClick} />
+        <Provider store={store}>
+          <Main offers={OFFERS_TESTS} selectedCity={CITIES_TESTS[0]} cities={CITIES_TESTS} onCityClick={() => {}} onOfferTitleClick={onOfferTitleClick} />
+        </Provider>
     );
 
     const allTitles = mainScreen.find(`h2.place-card__name`);
