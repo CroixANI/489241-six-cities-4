da7514d1103615321be465f5621b34f0981b1734
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-17 01:00:57 +0300

    6.21.8 Fix code review comments - Удалил CititesMenuWithActive и подключаю withActiveItem HOC в компоненте Main для CitiesMenu.

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import OffersCardsList from '../offers-cards-list/offers-cards-list.jsx';
-import CitiesMenuWithActive from '../cities-menu-with-active/cities-menu-wth-active.jsx';
+import CitiesMenu from '../cities-menu/cities-menu.jsx';
+import withActiveItem from '../../hocs/with-active-item/with-active-item.jsx';
 
 const Main = (props) => {
   const {offers, cities, selectedCity, onCityClick, onOfferTitleClick} = props;
   const mainClassName = `page__main page__main--index ${offers.length > 0 ? `` : `page__main--index-empty`}`;
+  // to test empty screen
+  const limitedCities = [`Düsseldorf`, ...cities].sort().slice(0, 6);
+
+  const CitiesMenuWithActive = withActiveItem(CitiesMenu);
+
   return (
     <>
       <div style={{display: `none`}}>
@@ -38,7 +44,7 @@ const Main = (props) => {
 
         <main className={mainClassName}>
           <h1 className="visually-hidden">Cities</h1>
-          <CitiesMenuWithActive cities={cities} selectedCity={selectedCity} onCityClick={onCityClick} />
+          <CitiesMenuWithActive items={limitedCities} activeItem={selectedCity} onItemSelected={onCityClick} />
           <OffersCardsList onOfferTitleClick={onOfferTitleClick} />
         </main>
       </div>
