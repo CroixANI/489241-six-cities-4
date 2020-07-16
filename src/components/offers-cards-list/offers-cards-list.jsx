cbf8caa0fcf7021f7070d8c457639d7fe54a5587
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-16 20:12:41 +0300

    6.21.6 Добавил компонент для пустого списка предложений и snapshot тест.

--- src/components/offers-cards-list/offers-cards-list.jsx
+++ src/components/offers-cards-list/offers-cards-list.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import OffersCardsContainer from '../offers-cards-container/offers-cards-container.jsx';
+import OffersCardsListEmpty from '../offers-cards-list-empty/offers-cards-list-empty.jsx';
 import Map from '../map/map.jsx';
 import {withClassName} from '../../hocs/with-class-name/with-class-name.jsx';
 import SortOffersMenu from '../sort-offers-menu/sort-offers-menu.jsx';
@@ -15,17 +16,7 @@ const OffersCardsList = (props) => {
 
   if (items.length === 0) {
     return (
-      <div className="cities">
-        <div className="cities__places-container cities__places-container--empty container">
-          <section className="cities__no-places">
-            <div className="cities__status-wrapper tabs__content">
-              <b className="cities__status">No places to stay available</b>
-              <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
-            </div>
-          </section>
-          <div className="cities__right-section"></div>
-        </div>
-      </div>
+      <OffersCardsListEmpty />
     );
   }
 
