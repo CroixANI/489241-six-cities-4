4e0d5c7ee72db281df07671f28f2953bdea1a933
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-18 12:34:56 +0300

    7.24.6 Вернул поддержку Redux DevTools.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -19,7 +20,9 @@ const api = createAPI(() => {
 
 const store = createStore(
     reducer,
-    applyMiddleware(thunk.withExtraArgument(api))
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 store.dispatch(ActionCreator.loadData([]));
