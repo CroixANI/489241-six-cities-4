1fc4a49a9e72ce911de4523537b8c0bbf5496c1b
a.nichiporovich <a.nichiporovich@gmail.com>
2020-07-27 23:01:44 +0300

    7.26.6 Блокирую форму на период отправки отзыва.

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -50,7 +50,8 @@ class ReviewForm extends PureComponent {
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
           <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-            required={true} checked={currentReview.rating === `5`} onChange={this._handleInputsChange} />
+            required={true} checked={currentReview.rating === `5`} onChange={this._handleInputsChange}
+            disabled={currentReview.isSubmitInProgress} />
           <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -58,7 +59,8 @@ class ReviewForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-            required={true} checked={currentReview.rating === `4`} onChange={this._handleInputsChange} />
+            required={true} checked={currentReview.rating === `4`} onChange={this._handleInputsChange}
+            disabled={currentReview.isSubmitInProgress} />
           <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -66,7 +68,8 @@ class ReviewForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
-            required={true} checked={currentReview.rating === `3`} onChange={this._handleInputsChange} />
+            required={true} checked={currentReview.rating === `3`} onChange={this._handleInputsChange}
+            disabled={currentReview.isSubmitInProgress} />
           <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -74,7 +77,8 @@ class ReviewForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
-            required={true} checked={currentReview.rating === `2`} onChange={this._handleInputsChange} />
+            required={true} checked={currentReview.rating === `2`} onChange={this._handleInputsChange}
+            disabled={currentReview.isSubmitInProgress} />
           <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -82,7 +86,8 @@ class ReviewForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
-            required={true} checked={currentReview.rating === `1`} onChange={this._handleInputsChange} />
+            required={true} checked={currentReview.rating === `1`} onChange={this._handleInputsChange}
+            disabled={currentReview.isSubmitInProgress} />
           <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -92,7 +97,7 @@ class ReviewForm extends PureComponent {
         <textarea className="reviews__textarea form__textarea" id="review" name="review"
           placeholder="Tell how was your stay, what you like and what can be improved"
           minLength="50" maxLength="300" required={true} onChange={this._handleInputsChange}
-          value={currentReview.reviewText}></textarea>
+          value={currentReview.reviewText} disabled={currentReview.isSubmitInProgress}></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
